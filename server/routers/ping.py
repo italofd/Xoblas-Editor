b4371d25e9834766f02f348c3119e6a2990d034c
italofd <italoferreiradutra@gmail.com>
2025-04-01 22:37:52 -0300

    Increment documentation for ping, using multiple response types

--- server/routers/ping.py
+++ server/routers/ping.py
@@ -1,9 +1,23 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
 
 router = APIRouter()
 
-@router.get("/ping", tags=["ping"])
-async def ping():
-    return {"Pong =)"}
+class SuccessPingResponse(BaseModel):
+    message: str
+
+@router.post("/ping", tags=["connection"], name="Ping", description="Call this endpoint to check if the server is up and running", responses={
+    200: {
+        "model": SuccessPingResponse
+    },
+    400: {
+        "description": "Any code above 400 means that the server is not running, this endpoint just return a simple string after all"
+    }
+})
+def ping():
+    return {
+        "message": "Pong =)"
+    }
 
 
