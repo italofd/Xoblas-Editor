2d77ace92eb47703eb26eb32cc4f2182b6db28e8
italofd <italoferreiradutra@gmail.com>
2025-04-02 15:23:00 -0300

    Change default python to use pandas and scypi example

--- web-app/src/constants/editor.ts
+++ web-app/src/constants/editor.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_PYTHON_CODE =
-	'def main():\n    print("Hello, world!")\n\nmain()';
+	"#You have access to both libraries with all modules\n#Import as it is below and feel free to use =)\nimport scipy\nimport pandas\n\n\ndef main():\n    mydataset = {\n        " +
+	'\'languages\': ["JS", "GO", "Python"],\n        \'speed\': [7, 8, 5]\n    }\n    print("Hello, world!")\n    print(pandas.DataFrame(mydataset))\n    print(scipy.constants.liter)\n\nmain()';
